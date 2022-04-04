c4b24f368387ddfdd118dc7d46a1d40baa31e190
10_2pang <57481378+yedol1@users.noreply.github.com>
2022-04-04 19:30:37 +0900

    covid issue

--- HTML&CSS/잔재미코딩_강의/잔재미코딩_연습/js/main.js
+++ HTML&CSS/잔재미코딩_강의/잔재미코딩_연습/js/main.js
@@ -70,25 +70,10 @@ function transformPrev(event) {
     let activeLi = classList.getAttribute('data-position');
     const liList = classList.getElementsByTagName('li');
 
-  /* classList.clientWidth 는 ul 태그의 실질적인 너비
-   * liList.length * 260 에서 260 은 각 li 요소의 실질 너비(margin 포함)
-   * activeLi 는 data-position 에 있는 현재 위치 
-   * 즉, liList.length * 260 + Number(activeLi) 는 현재 위치부터 오른쪽으로 나열되야 하는 나머지 카드들의 너비
-   */
-
-  /* classList.clientWidth < (liList.length * 260 + Number(activeLi)) 의미는
-   * 오른쪽으로 나열될 카드들이 넘친 상태이므로, 왼쪽으로 이동이 가능함
-   */
-  
    if (classList.clientWidth < (liList.length * 260 + Number(activeLi))) {
-       // 위치를 왼쪽으로 260 이동 (-260px)
+       
        activeLi = Number(activeLi) - 260;
 
-       /* 위치를 왼쪽으로 260 이동 (-260px)
-       * 해당 위치는 변경된 activeLi 값이 적용된 liList.length * 260 + Number(activeLi) 값임
-       * 이 값보다, classList.clientWidth (ul 태그의 너비)가 크다는 것은
-       * 넘치는 li 가 없다는 뜻으로, NEXT 버튼은 활성화되면 안됨
-       */
       if (classList.clientWidth > (liList.length * 260 + Number(activeLi))) {
         slidePrev.style.color = '#cfd8dc';
         slidePrev.classList.remove('slide-prev-hover');
@@ -108,20 +93,13 @@ function transformPrev(event) {
 const slidePrevList = document.getElementsByClassName('slide-prev');
 
 for (let i = 0; i < slidePrevList.length; i++) {
-    // ul 태그 선택
     let classList = slidePrevList[i].parentElement.parentElement.nextElementSibling;
     let liList = classList.getElementsByTagName('li');
 
-    // 카드가 ul 태그 너비보다 넘치면, 왼쪽(PREV) 버튼은 활성화하고, 오른쪽(NEXT)는 현재 맨 첫카드 위치이므로 비활성화
     if (classList.clientWidth < (liList.length * 260)) {
         slidePrevList[i].classList.add('slide-prev-hover');
         slidePrevList[i].addEventListener('click', transformPrev);
     } else {
-        /* 카드가 ul 태그 너비보다 넘치지 않으면, PREV, NEXT 버튼 불필요하므로, 아예 삭제함
-        태그 삭제시, 부모 요소에서 removeChild 를 통해 삭제해야 함
-           따라서, 1. 먼저 부모 요소를 찾아서,
-                 2. 부모 요소의 자식 요소로 있는 PREV 와 NEXT 요소를 삭제함
-        */
        const arrowContainer = slidePrevList[i].parentElement;
        arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
        arrowContainer.removeChild(slidePrevList[i]);
